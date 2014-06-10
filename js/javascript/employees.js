b9a2fcbf683f0fb70b65e5a487f52d6e91b3b5b4
em222iv <erikmagnusson@eriks-imac.lan>
2014-06-10 20:55:59 +0200

    test

--- js/javascript/employees.js
+++ js/javascript/employees.js
@@ -1,3 +1,30 @@
 /**
  * Created by erikmagnusson on 2014-06-10.
  */
+var employees = {
+    employeeArray : []
+}
+
+function employed() {
+
+    var person1 = {
+        Name:"Stefan Snyting"
+    };
+    var person2 = {
+        Name:"Michaelaaaaa"
+    };
+
+
+    employees.employeeArray[0] = person1;
+    employees.employeeArray[1] = person2;
+
+    appendEmplyees();
+}
+function appendEmplyees() {
+    var weekDiv = document.getElementById('edit-monday1');
+
+    weekDiv.innerHTML = employees.employeeArray.toString();
+
+}
+
+window.onload = employed();
