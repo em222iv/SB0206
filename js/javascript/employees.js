8a85902f57d8e42c4f5c15689bee0cf1062b4cb2
em222iv <erikmagnusson@eriks-imac.lan>
2014-06-11 17:36:06 +0200

    employee add effect, query. soon remove

--- js/javascript/employees.js
+++ js/javascript/employees.js
@@ -2,29 +2,65 @@
  * Created by erikmagnusson on 2014-06-10.
  */
 var employees = {
-    employeeArray : []
+    employeeArray : [],
+    workingArraysArray : [],
+    e : null,
+    aTagArray : []
 }
 
 function employed() {
-
-    var person1 = {
-        Name:"Stefan Snyting"
-    };
-    var person2 = {
-        Name:"Michaelaaaaa"
-    };
-
-
-    employees.employeeArray[0] = person1;
-    employees.employeeArray[1] = person2;
+    employees.employeeArray[0] = "Stefan Snyting";
+    employees.employeeArray[1] = "Michaela Gladersson";
+    employees.employeeArray[2] = "Alfonso Blomma";
+    employees.employeeArray[3] = "Malin Vikings";
+    employees.employeeArray[4] = "Ers majestät";
+    employees.employeeArray[5] = "Hermione Telefongluttare";
 
     appendEmplyees();
 }
 function appendEmplyees() {
-    var weekDiv = document.getElementById('edit-monday1');
+    var weekDiv = document.getElementById('edit-monday1add');
+    var aTagCounter = 0;
+    employees.employeeArray.forEach(function(employee) {
+        aTagCounter++;
+        employees.e = employee;
+        var a = document.createElement('a');
+        a.setAttribute('class','small-1 columns');
 
-    weekDiv.innerHTML = employees.employeeArray.toString();
+        var aTagNumber = a.setAttribute('id', 'aTag' + aTagCounter);;
 
+
+        employees.aTagArray.push('aTag' + aTagCounter);
+
+        weekDiv.appendChild(a);
+        a.innerHTML = employees.e.toString();
+
+    });
+    clickedEmployee();
 }
+
+function clickedEmployee(aTagNumber) {
+    $('#edit-monday1add').on('click', function(e) {
+
+        $("a").click(function(event) {
+            var clickedTag = event.target.id;
+            addWorker(clickedTag);
+        });
+    });
+    $('#edit-monday1remove').on('click', function(e) {
+
+        $("a").click(function(event) {
+            var clickedTag = event.target.id;
+            removeWorker(clickedTag);
+        });
+    });
+}
+function addWorker(clickedTag) {
+    $( "#" + clickedTag ).remove();
+};
+
+function removeWorker() {
+
+};
 
 window.onload = employed();
